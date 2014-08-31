e9fc0cfba7bdfcf4ee004c0f4c6d3f9afe7517eb
lhunker <lukas.hunker@gmail.com>
2014-08-31 14:54:11 -0400

    Adding statistics calculation to run after command is called.

--- doit.cpp
+++ doit.cpp
@@ -5,8 +5,12 @@ using namespace std;
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/resource.h>
+#include <sys/time.h>
 
 void runCommand(char *args[]);
+void getStats();
+double timevalToMs (struct timeval time);
 
 int main (int argc, char *argv[]){
 	if (argc > 1){	//temporary
@@ -16,10 +20,48 @@ int main (int argc, char *argv[]){
 		}
 		newargs[argc -1] = 0;
 		runCommand(newargs);
-		wait(0);
+		getStats();
 	}
-	cout << "done\n";
 	exit(0);
+}
+
+/*
+ * Finds and prints statistics of the completed child process
+ */
+void getStats(){
+	struct rusage usage;
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+	wait(0);	//Wait for child to complete
+	gettimeofday(&end, NULL);
+	if(getrusage(RUSAGE_CHILDREN, &usage) != 0){
+        	cerr << "Error getting usage\n";
+        }
+	double utime = timevalToMs(usage.ru_utime);
+	double stime = timevalToMs(usage.ru_stime);
+	double wtime = timevalToMs(end) - timevalToMs(start);
+	cout << "Statistics\n";
+	cout << "Wall time : " << wtime << "ms\n";
+        cout << "User Time: " << utime  << "ms\n";
+	cout << "System Time : " << stime << "ms\n";
+	cout << "Involuntarily preempted " << usage.ru_nivcsw << " times\n";
+	cout << "Voluntarily preempted " << usage.ru_nvcsw << " times\n";
+	cout << "Page faults : " << usage.ru_majflt << "\n";
+	cout << "Page faults satisfied by memory reclaim: " << usage.ru_minflt << "\n";
+
+}
+
+/*
+ * Converts a timeval struct to milliseconds
+ * 
+ * Params:
+ * 	time - the time to be converted
+ *
+ * Returns:
+ * 	The timeval in milliseconds as a double
+ */
+double timevalToMs(struct timeval time){
+	return time.tv_sec * 1000 + (time.tv_usec/1000.0);
 }
 
 /*
